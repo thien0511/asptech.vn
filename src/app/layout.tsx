9ee2f133cf847b3e0ad768e559f25faa54fe29ae
Thien Nguyen Duc <nguyendwcthien@gmail.com>
2026-07-11 19:36:15 +0700

    update icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,13 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: { default: "ASP Tech", template: "%s | ASP Tech" },
   description: "Công ty Cổ phần Đầu tư và Chuyển giao Công nghệ ASP",
+  icons: {
+    icon: [
+      { url: "/favicon.ico", sizes: "any" },
+      { url: "/icon.png", type: "image/png", sizes: "512x512" },
+    ],
+    apple: [{ url: "/apple-icon.png", type: "image/png", sizes: "180x180" }],
+  },
 };
 
 export default function RootLayout({
